7a83b32009e8645d67eb06ba3c388d08fba5c1aa
Paul Stork <paul.michael.stork@gmail.com>
2025-02-05 22:46:10 -0500

    update libraries and refactor code

--- coinAPI-pull.py
+++ coinAPI-pull.py
@@ -17,9 +17,8 @@ import requests
 import json
 import pandas as pd
 from datetime import datetime, timedelta
-from sqlalchemy import exc
-from sqlalchemy import create_engine
-
+from sqlalchemy import exc, create_engine
+import time
 
 load_dotenv()
 coinapi_key = os.getenv('coinapi_key')
@@ -28,8 +27,6 @@ conn_string = os.getenv('db_conn_string')
 
 db = create_engine(conn_string)
 conn = db.connect()
-
-
 
 # Get the date for today and yesterday and convert them to YYYY-mm-ddT00:00:00 format for API call
 yesterday_date = datetime.now() - timedelta(days = 1)
@@ -42,8 +39,9 @@ daily_df = pd.DataFrame()
 i = 1
 
 # Get yesterdays crypto coin values
-crypto_coins = {1:"BTC", 2:'ETH', 3:'USDT', 4:"USDC", 5:"BNB", 6:"BUSD", 7:"XRP", 8:"ADA", 9:"DOGE", 10:"MATIC"}
-while i <= len(crypto_coins):
+crypto_coins = {0:"BTC", 1:'ETH', 2:'USDT', 3:"USDC", 4:"BNB", 5:"BUSD", 6:"XRP", 7:"ADA", 8:"DOGE", 9:"MATIC"}
+# while i <= len(crypto_coins):
+for i in range(len(crypto_coins)):
     exchange_id = crypto_coins[i]
 
     url = f'https://rest.coinapi.io/v1/exchangerate/{exchange_id}/USD/history?period_id=30MIN&time_start={yesterday}&time_end={today}'
@@ -51,22 +49,26 @@ while i <= len(crypto_coins):
         yesterdays_response = requests.get(f'{url}&apikey={coinapi_key}')
     
         daily_text = json.dumps(yesterdays_response.json(), sort_keys=True, indent=4)
-
-        temp_df = pd.read_json(daily_text)
-        temp_df['exchange_id'] = exchange_id
-        daily_df = pd.concat([daily_df, temp_df])
-        
-        i += 1
+        print(daily_text)
+        if "status" not in daily_text:
+            temp_df = pd.read_json(daily_text)
+            temp_df['exchange_id'] = exchange_id
+            daily_df = pd.concat([daily_df, temp_df])
+            
+            time.sleep(3)
+        else: 
+            time.sleep(15)
     except requests.HTTPError as e:
         print(f'There was an error: {e}')
 
 
 daily_df = daily_df[['exchange_id','rate_close','rate_high','rate_low','rate_open','time_close','time_open','time_period_end','time_period_start']]
 
+print(daily_df)
 
-try:    
-    daily_df.to_sql('daily_crypto_data', schema = 'crypto_data', con=conn, if_exists='append', index=False)
+# try:    
+#     daily_df.to_sql(name = 'daily_crypto_data', con=conn, if_exists='append', index=False)
 
-    print('data loaded to table')
-except exc.SQLAlchemyError as e:
-    print(f'There was an error: {e}')
+#     print('data loaded to table')
+# except exc.SQLAlchemyError as e:
+#     print(f'There was an error: {e}')
